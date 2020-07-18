a6a0a27bf3c1ef88e3eed181e8f6d1b3df8950ce
Arya Nguyen <49668914+AryaNguyen@users.noreply.github.com>
2020-07-18 19:25:41 -0400

    Update main.cpp: able to open camera but can only read 1 frame

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+
 #include "WebcamStream.h"
+#include "utils.h"
 
-int main() {
-    WebcamStream ws(0);
-    // ws.start();
+int main() 
+{
+    int numFrames = 100;  // default
+    std::cout << "Start1\n";
+    WebcamStream ws;
+    FPS fps;
+    cv::Mat frame;
 
-    // ws.stop();
+    // start the program
+    ws = WebcamStream().start();
+    fps.start();
+    std::cout << "Start2\n";
+    while (fps.getNumFrames() < numFrames) {
+        frame = ws.read();
+
+        cv::imshow("Live", frame);
+        if (cv::waitKey(5) >= 0) {
+            break;
+        }
+
+        fps.update();
+    }
+    std::cout << "out of while loop";
+    // stop the frogram
+    fps.stop();
     cv::destroyAllWindows();
+    ws.stop();
+
     return 0;
 }
